31c964116ad640d4d7d4773ee87d338e3bacec4c
GeronimoNeto <102226234+GeronimoNeto@users.noreply.github.com>
2022-09-14 15:01:21 -0300

    react

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/index.css';
 import App from './js/App';
-import reportWebVitals from './js/reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,5 +9,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-reportWebVitals();
