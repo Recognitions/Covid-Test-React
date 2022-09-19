f2e6b22b4be0ea8cb24e7ebb0b01610462822d9e
GeronimoNeto <102226234+GeronimoNeto@users.noreply.github.com>
2022-09-19 16:59:09 -0300

    log

--- src/js/components/Consult.js
+++ src/js/components/Consult.js
@@ -5,13 +5,10 @@ import symp from './Symp'
 
 async function consult(e){
     e.preventDefault()
-    
     const consultForm = document.getElementById("consultForm").dataset.id
     const result = document.getElementById("consultForm").dataset.result
     const patientSymp = document.getElementById("consultForm").dataset.symp
     const sympPlus = document.getElementById("consultForm").dataset.sympPlus
-
-    console.log(consultForm,result,patientSymp,sympPlus)
 
     const consult = await api.get(`/patient/consult/${consultForm}/${result}/${patientSymp}/${sympPlus}`)
     if(consult.data){
