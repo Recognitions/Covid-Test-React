6798b1e226ea9a0d5ddc476746e204aa1fa3bdbd
GeronimoNeto <102226234+GeronimoNeto@users.noreply.github.com>
2022-09-21 16:18:42 -0300

    Pagination

--- src/js/components/Pagination.js
+++ src/js/components/Pagination.js
@@ -1,11 +1,8 @@
-import { render } from "../Create"
-
 function pagination(array,actual,limit){
     let result = []
     let total = Math.ceil(array.length/limit)
     let count = (actual*limit)-limit
     let delimit = count+limit
-
     if(actual<=total){
         for(let i=count; i<delimit; i++){
             result.push(array[i])
